f05b42ba17c7c20d8f5cdbd60b7c00262e16cbb5
ayselkuru <157073429+ayselkuru@users.noreply.github.com>
2024-05-02 12:36:01 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,2 +1,18 @@
 #! /bin/sh
-echo "Hello world"
+mkdir output
+cp aysel.txt output 
+cd output 
+cat aysel.txt > read.txt 
+pwd > pwd.txt 
+ls > ls.txt 
+cp aysel.txt copy.txt 
+alias today="date +'%Y-%m-%d'"
+wc -w aysel.txt > textcount.txt 
+ps | head -n 5 > process.txt
+ifconfig | head -n 5 > netstat.txt
+mount | head -n 5 > mount.txt
+touch permissions.txt
+chmod a+rwx permissions.txt
+export TESTENV1="test"
+grep -E '\b\w{3,}\b' aysel.txt > regex.txt
+cd ..
